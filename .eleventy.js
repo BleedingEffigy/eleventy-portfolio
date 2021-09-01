10b52ccb70fb294cd20e517ebeda3079baa6ac60
phantomhaze <ahern077@gmail.com>
2021-09-01 15:47:56 -0700

    removed images from pass through, tracking css

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
   // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
-  eleventyConfig.addPassthroughCopy("src/img");
   // Set custom directories for input, output, includes, and data
   return {
     dir: {
