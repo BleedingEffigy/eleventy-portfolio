708bb2fc9dbdd4226fb3ce7e2fa256a164d708a6
phantomhaze <ahern077@gmail.com>
2021-09-13 06:36:15 -0700

    added purgecss plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,18 @@
+const purgeCssPlugin = require("eleventy-plugin-purgecss");
+
+
 module.exports = function(eleventyConfig) {
   // Copy `src/style.css` to `_site/style.css`
   eleventyConfig.addPassthroughCopy("src/style.css");
   // passthrough `dynamic-animations.js`
   eleventyConfig.addPassthroughCopy("src/dynamic-animations.js");
+  // add purgeCss plugin
+  eleventyConfig.addPlugin(purgeCssPlugin, {
+    // Optional: Specify the location of your PurgeCSS config
+    config: "./purgecss.config.js",
+    // Optional: Set quiet: true to suppress terminal output
+    quiet: false,
+  });
   // Set custom directories for input, output, includes, and data
   return {
     dir: {
